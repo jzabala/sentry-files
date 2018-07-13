9c292bbcd809882b5f35581a87d410b015ab8c0f
Johnny Zabala <jzabala.s@gmail.com>
2018-07-13 18:59:52 +0200

    feat(sentry): add sentry service
    add release creating and file uploading functions based on configuration.

---
+++ src/sentryService.js
@@ -0,0 +1,54 @@
+const { getReleaseUrl, getFilesUrl } = require('./helpers')
+const { post } = require('./sentryClient')
+const ClientError = require('./clientError')
+
+function service(config) {
+  const { version, account, project, token } = config
+
+  function createRelease() {
+    return new Promise((resolve, reject) => {
+      post(getReleaseUrl(account, project))
+        .setToken(token)
+        .setBody({ version })
+        .end((error, response) => {
+          if (error) {
+            if (error instanceof ClientError) {
+              const responseError = new Error()
+              responseError.data = error.response.body
+              return reject(responseError)
+            }
+            return reject(error)
+          }
+          const { body } = response
+          return resolve(body)
+        })
+    })
+  }
+  function uploadFile(file) {
+    return new Promise((resolve, reject) => {
+      post(getFilesUrl(account, project, version))
+        .setToken(token)
+        .attachFile(file)
+        .end((error, response) => {
+          if (error) {
+            if (error instanceof ClientError) {
+              const responseError = new Error()
+              responseError.data = Object.assign({}, error.response.body, {
+                file
+              })
+              return reject(responseError)
+            }
+            return reject(error)
+          }
+          const { body } = response
+          return resolve(body)
+        })
+    })
+  }
+  return {
+    createRelease,
+    uploadFile
+  }
+}
+
+module.exports = service
